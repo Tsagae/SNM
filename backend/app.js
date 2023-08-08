9d0d3b958e087f5eea6a4bfa1e2b8e9afa2d9dbc
Matteo Zagheno <matteo2309@hotmail.com>
2023-08-08 15:05:14 +0200

    added initial jwt auth

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const auth = require('./authentication')
 const express = require('express')
 const cors = require('cors');
 const config = require('config');
@@ -5,8 +6,9 @@ const app = express();
 const port = config.get('server.port');
 const host = config.get('server.host');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { generateSecret } = require('./authentication');
 const uri = "mongodb+srv://" + config.get('db.user') + ":" + config.get('db.pass') + "@" + config.get('db.cluster') + "/?retryWrites=true&w=majority";
-
+const authSecret = config.get('auth.secret');
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,8 +26,8 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    await  listDatabases(client);
-    
+    await listDatabases(client);
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -33,7 +35,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-async function listDatabases(client){
+async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
 
   console.log("Databases:");
@@ -43,9 +45,21 @@ async function listDatabases(client){
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send({ text: "Hello World" });
+  res.send({ text: "Hello World" });
+});
+
+
+app.get('/getToken', (req, res) => {
+  res.status(200).send({ token: auth.generateAccessToken("testName", authSecret) });
+});
+
+
+app.get('/authToken', (req, res) => {
+  auth.authenticateToken(req, res, authSecret);
 });
 
 app.listen(port, host, () => {
   console.log(`Server is running on ${host}:${port}`);
 });
+
+
