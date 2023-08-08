5487c3367c617880b21b6b0e7795da15c4d11b0e
Matteo Zagheno <matteo2309@hotmail.com>
2023-08-08 11:46:09 +0200

    added cors module

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send({ text: "Hello World" });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
