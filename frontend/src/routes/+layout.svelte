bf1b1fa5007667e154ee3016d342dfe7243f9b0f
Catu42 <provinimartina@gmail.com>
2023-08-08 22:48:09 +0200

    Starting with some style

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,52 @@
-<h1>SNM</h1>
-<nav>
-	<a href="/">Home</a>
-	<a href="/registration">Registration</a>
-	<a href="/about">About</a>
-	<a href="/settings">Settings</a>
-</nav>
+<script>
+	import { Navbar, Nav, NavItem, NavLink, NavbarBrand } from 'sveltestrap';
+	import { fly } from 'svelte/transition';
+	import { onMount } from 'svelte';
+  
+	let ready = false;
+	onMount(() => ready = true);
+</script>
 
-<a href="/hello-world">hello-world</a>
+<Navbar color="success" light>
+	{#if ready}
+	<h1 transition:fly={{ x: 200, duration: 1500 }} >
+	SNM
+	</h1>
+	{/if}
+</Navbar>
+
+<Navbar color="light" light expand="md">
+	<NavbarBrand href="/">Home</NavbarBrand>
+	  <Nav class="ms-auto" navbar>
+		<NavItem>
+			<NavLink href="/registration">Registration</NavLink>
+		</NavItem>
+		<NavItem>
+			<NavLink href="/hello-world">Hello World!</NavLink>
+		</NavItem>
+		<NavItem>
+		  <NavLink href="/login">Login</NavLink>
+		</NavItem>
+		<NavItem>
+		  <NavLink>Logout</NavLink>
+		</NavItem>
+	  </Nav>
+  </Navbar>
+
+<div class="page-content">
 
 <slot />
+
+</div>
+
+<style>
+	
+	:global(body) {
+		background-color: #535353;
+		color: #1db954;
+	}
+
+    .page-content {
+        padding: 20px;
+    }
+</style>
