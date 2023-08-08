85a0c9bd6aec7f378953dc72bf03f7e3176c5645
Matteo Zagheno <matteo2309@hotmail.com>
2023-08-08 11:49:52 +0200

    added example hello-world

---
+++ frontend/src/routes/hello-world/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	async function fetchData() {
+		const res = await fetch(`http://localhost:3000`);
+		return await res.json();
+	}
+
+	let lateData;
+	fetchData().then((lateRes) => (lateData = lateRes.text));
+</script>
+
+<h1>Hello and welcome to my site!</h1>
+<a href="/about">About my site</a>
+
+{#await fetchData()}
+	<p>waiting</p>
+{:then data}
+	<p>{data.text}</p>
+{/await}
+
+<p>{lateData == null ? 'waiting' : lateData}</p>
+
+<style>
+</style>
