4a8c562047f894ee14d51d0e78bba2f870963d14
Matteo Zagheno <matteo2309@hotmail.com>
2023-08-11 13:24:58 +0200

    added  successful registration feedback

--- frontend/src/routes/registration/+page.svelte
+++ frontend/src/routes/registration/+page.svelte
@@ -1,90 +1,109 @@
 <script>
-	import { useForm, Hint, HintGroup, validators, required, minLength, email } from 'svelte-use-form';
-	import { passwordMatch, containNumbers } from "./customValidators";
-	
+	import {
+		useForm,
+		Hint,
+		HintGroup,
+		validators,
+		required,
+		minLength,
+		email
+	} from 'svelte-use-form';
+	import { passwordMatch, containNumbers } from './customValidators';
+
 	const form = useForm();
-	
-	const requiredMessage = "This field is required";
 
-  async function onSubmit() {
-	console.log("form: ", $form);
-	const data = {
-		email: $form.email.value,
-		username: $form.name.value,
-		password: $form.password.value,
-	};
-    console.log("data: ", JSON.stringify(data));
-	await registerUser(data);
-  }
+	const requiredMessage = 'This field is required';
 
-  async function registerUser (user) {
+	let hasRes = false;
+	let resOk;
+
+	async function onSubmit() {
+		console.log('form: ', $form);
+		const data = {
+			email: $form.email.value,
+			username: $form.username.value,
+			password: $form.password.value
+		};
+		console.log('data: ', JSON.stringify(data));
+		await registerUser(data);
+	}
+
+	async function registerUser(user) {
 		const res = await fetch('http://localhost:3000/register', {
 			method: 'POST',
 			headers: {
-			"Content-Type": "application/json",
+				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(user)
-		})
-		
-		const json = await res.json()
-		let result = JSON.stringify(json)
+		});
+
+		const json = await res.json();
+		let result = JSON.stringify(json);
 		console.log(result);
+
+		hasRes = true;
+		resOk = res.ok;
 	}
 </script>
 
 <main>
 	<form use:form method="post">
-		<h1>
-			Registration
-		</h1>
+		<h1>Registration</h1>
 		<label for="email">Email</label>
 		<input type="email" name="email" use:validators={[required, email]} />
 		<HintGroup for="email">
 			<Hint on="required">{requiredMessage}</Hint>
-			<Hint on="email" hideWhenRequired>This must be a valid email</Hint>	
+			<Hint on="email" hideWhenRequired>This must be a valid email</Hint>
 		</HintGroup>
 
-		<label for="name">Name</label>
-		<input type="text" name="name"  />
+		<label for="username">Username</label>
+		<input type="text" name="username" />
 
 		<label for="password">Password</label>
-		<input type="password" name="password" use:validators={[required, minLength(5), containNumbers(2)]} />
+		<input
+			type="password"
+			name="password"
+			use:validators={[required, minLength(5), containNumbers(2)]}
+		/>
 		<HintGroup for="password">
 			<Hint on="required">{requiredMessage}</Hint>
-			<Hint on="minLength" hideWhenRequired let:value>This field must have at least {value} characters.</Hint>	
+			<Hint on="minLength" hideWhenRequired let:value
+				>This field must have at least {value} characters.</Hint
+			>
 			<Hint on="containNumbers" hideWhen="minLength" let:value>
 				This field must contain at least {value} numbers.
-			</Hint>	
+			</Hint>
 		</HintGroup>
 
 		<label for="passwordConfirmation">Password Confirmation</label>
 		<input type="password" name="passwordConfirmation" use:validators={[required, passwordMatch]} />
 		<HintGroup for="passwordConfirmation">
 			<Hint on="required">{requiredMessage}</Hint>
-			<Hint on="passwordMatch" hideWhenRequired>Passwords do not match</Hint>	
+			<Hint on="passwordMatch" hideWhenRequired>Passwords do not match</Hint>
 		</HintGroup><br />
 
-		<button disabled={!$form.valid} on:click|preventDefault={onSubmit}>
-			Submit
-		</button>
+		<button disabled={!$form.valid} on:click|preventDefault={onSubmit}> Submit </button>
 	</form>
+
+	{#if hasRes}
+		<p>{resOk ? 'Registration successful' : 'Registration failed'}</p>
+	{/if}
 	<pre>
 		{JSON.stringify($form, null, 1)}
 	</pre>
 </main>
-	
 
 <style>
 	:global(.touched:invalid) {
 		border-color: red;
 		outline-color: red;
 	}
-	
+
 	main {
 		display: flex;
 		justify-content: space-around;
 	}
-	
+
 	pre {
 		height: 80vh;
 		overflow: auto;
