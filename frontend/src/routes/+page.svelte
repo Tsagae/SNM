227f8cc8ead284b0b32d341196334ea8cc39a8d4
Catu42 <93785837+Catu42@users.noreply.github.com>
2023-09-30 10:46:54 +0200

    add filter params

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,13 +10,19 @@
         CardText,
         CardTitle } from 'sveltestrap';
     import { onMount } from 'svelte';
-    import { fly } from 'svelte/transition';
+    import { fly, slide } from 'svelte/transition';
 
     let ready = false;
 	onMount(() => ready = true);
 
     let keyword = '';
+    let visible = false;
 
+    function toggleVisible() {
+        visible = !visible
+    }
+
+    // array di canzoni di prova, da sostituire con i risultati da spotify
     let songs = [
 		{ id: '001', name: 'song1' },
 		{ id: '002', name: 'song2' },
@@ -35,6 +41,23 @@
         <p>Searching playlist by {keyword || '...'}</p>
 
         <input bind:value={keyword} placeholder="enter keyword" />
+        <br><br>
+        Filters
+        {#if visible}
+            <img class="buttImgRev" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Green_Arrow_Down.svg/2048px-Green_Arrow_Down.svg.png" on:click={toggleVisible} alt="showFilters">
+        {:else}
+            <img class="buttImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Green_Arrow_Down.svg/2048px-Green_Arrow_Down.svg.png" on:click={toggleVisible} alt="showFilters">
+        {/if}
+        
+        <br><br>
+        {#if visible}
+            <div style="width: 100%; text-align:center;" in:fly={{ y: -50, duration: 1000 }} out:fly={{ y: -50, duration: 1000 }}>
+                <input type="checkbox" id="album" name="album" checked /><label for="album">Album</label>
+                <input type="checkbox" id="artist" name="artist" checked /><label for="artist">Artist</label>
+                <input type="checkbox" id="playlist" name="playlist" checked /><label for="playlist">Playlist</label>
+                <input type="checkbox" id="track" name="track" checked /><label for="track">Track</label>
+            </div>
+        {/if}
 
     </div> 
 </Container>
@@ -85,5 +108,18 @@
         width: 100%; 
         max-width: 500px; 
         max-height: 300px;
+    }
+    .buttImgRev{
+        width:20px; 
+        transform:rotate(180deg);
+    }
+    .buttImgRev:hover{
+        filter: drop-shadow(3px 3px 2px #ffffff);
+    }
+    .buttImg{
+        width:20px; 
+    }
+    .buttImg:hover{
+        filter: drop-shadow(3px 3px 2px #ffffff);
     }
 </style>
