819ab4e039d0e4d90894a2f21fecbf4aa950b76a
Matteo Zagheno <matteo2309@hotmail.com>
2023-09-30 18:43:27 +0200

    Added repositories functions and changed authenticateRequest mechanism

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,14 @@ const express = require('express')
 const cors = require('cors');
 const config = require('config');
 
+// Repositories
+const albums = require('./services/repositories/albums');
+const artists = require('./services/repositories/artists');
+const generic = require('./services/repositories/generic');
+const playlists = require('./services/repositories/playlists');
+const tracks = require('./services/repositories/tracks');
+const users = require('./services/repositories/users');
+
 const app = express();
 const port = config.get('server.port');
 const host = config.get('server.host');
@@ -27,15 +35,72 @@ app.post('/login', validation.loginValidate, auth.login);
 
 
 app.post('/authToken', (req, res) => {
-    if (auth.authenticateRequest(req, res) != null) return;
+    if (!auth.authenticateRequest(req, res).authenticated) return;
     res.send({result: "valid token"});
 });
 
 //Spotify
 app.get('/search', (req, res) => {
-    if (auth.authenticateRequest(req, res) != null) return;
-    return spotify.search(req.query.q, req.query.filters).then((results) => res.send(results));
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return generic.search(req.query.q, req.query.filters).then((results) => res.send(results));
 });
+
+
+// -------- Tracks --------
+app.get('/getTrack', (req, res) => {
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return tracks.getTrack(req.query.id).then((results) => res.send(results));
+});
+
+app.get('/getTracks', (req, res) => {
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return tracks.getTracks(req.query.ids).then((results) => res.send(results));
+});
+
+
+// -------- Albums --------
+app.get('/getAlbum', (req, res) => {
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return albums.getAlbum(req.query.id).then((results) => res.send(results));
+});
+
+
+// -------- Artists --------
+app.get('/getArtist', (req, res) => {
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return artists.getArtist(req.query.id).then((results) => res.send(results));
+});
+
+// -------- Playlists --------
+app.get('/getPlaylist', (req, res) => {
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return playlists.getPlaylist(req.query.id).then((results) => res.send(results));
+});
+
+app.get('/getAllPublicPlaylists', (req, res) => {
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return playlists.getAllPublicPlaylists().then((results) => res.send(results));
+});
+
+app.get('/editPlaylist', (req, res) => {
+    let authReq = auth.authenticateRequest(req, res);
+    if (!authReq.authenticated) return;
+    return playlists.editPlaylist(req.query.id, authReq.user.username, req.query.name, req.query.isPublic, req.query.tracks).then((results) => res.send(results)).catch((e) => {
+        return res.status(400).send({error: e.message});
+    });
+});
+
+app.get('/createPlaylist', (req, res) => {
+    let authReq = auth.authenticateRequest(req, res);
+    if (!authReq.authenticated) return;
+    return playlists.createPlaylist(authReq.user.username, req.query.name, req.query.isPublic, req.query.tracks).then((results) => res.send(results));
+});
+// -------- Users --------
+app.get('/getUser', (req, res) => {
+    if (!auth.authenticateRequest(req, res).authenticated) return;
+    return users.getUser(req.query.id).then((results) => res.send(results));
+});
+
 
 app.listen(port, host, () => {
     console.log(`Server is running on ${host}:${port}`);
