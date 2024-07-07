16f0af4f7dbe0966bb855234b37a74636dcc590a
Catu42 <93785837+Catu42@users.noreply.github.com>
2024-07-07 18:28:44 +0200

    setup user playlist

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -80,7 +80,7 @@
 							</svelte:fragment>
 						</SidebarItem>
 						{#if logged}
-							<SidebarItem label="Playlist" href="/">
+							<SidebarItem label="Playlist" href="/personal">
 								<svelte:fragment slot="icon">
 									<CaretRightSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
 								</svelte:fragment>
---
+++ frontend/src/routes/personal/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+    import {
+        Heading,
+        Spinner,
+        Span
+    } from 'flowbite-svelte';
+    import {ChevronDownOutline, FilterOutline} from 'flowbite-svelte-icons';
+    import {myPlaylists} from '$lib/backend.js';
+    import Playlist from '$lib/components/playlist.svelte';
+    import {goto} from "$app/navigation";
+
+    let myName = localStorage.getItem('username');
+
+</script>
+
+<div class="container">
+
+    <Heading tag="h2" class="flex items-center m-4" size="text-5xl">
+        <Span gradient>Playlist</Span> Create
+    </Heading>
+
+    {#await myPlaylists()}
+        <div class="text-center">
+            <Spinner size={8} color="green"/>
+        </div>
+    {:then playlist}
+        {#each playlist as {_id, name, user, tracks, tags, thumbnail}, i}
+            <Playlist id={_id} name={name} user={myName} tracks={tracks} tags={tags} thumbnail={thumbnail} i={i}/>
+            <br><br>
+        {/each}
+    {/await}
+
+    <Heading tag="h2" class="flex items-center m-4" size="text-5xl">
+        <Span gradient>Playlist</Span> Salvate
+    </Heading>
+
+    {#await myPlaylists()}
+        <div class="text-center">
+            <Spinner size={8} color="green"/>
+        </div>
+    {:then playlist}
+        {#each playlist as {_id, name, user, tracks, tags, thumbnail}, i}
+            <Playlist id={_id} name={name} user={myName} tracks={tracks} tags={tags} thumbnail={thumbnail} i={i}/>
+            <br><br>
+        {/each}
+    {/await}
+
+</div>
