dd2e845c59e215f474d41ffcef62994bcfad21af
catu <martina.provini@studenti.unimi.it>
2024-07-08 15:25:11 +0200

    community page setup

---
+++ frontend/src/routes/community/[slug]/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import {
+        Heading,
+        Spinner
+    } from 'flowbite-svelte';
+
+    let pageInfo = window.location.pathname;
+    let id = pageInfo.replace("/community/", "");
+    id = id.replace("/", "");
+    let groupInfoPromise = getGroup(id);
+
+
+    
+</script>
+
+{#await groupInfoPromise}
+    <div class="text-center">
+        <Spinner size={8} color="green"/>
+    </div>
+{:then results}
+
+    <Heading tag="h1" class="w-full mt-6 mb-4 text-center">Nome Gruppo</Heading>
+
+    <div class="flex">
+        
+        <div class="w-2/3" style="background-color:red;">
+
+            Playlist condivise
+        
+        </div>
+
+        <div class="w-1/3" style="background-color:green;">
+
+            Elenco utenti
+        
+        </div>
+
+    </div>
+
+
+{/await}
