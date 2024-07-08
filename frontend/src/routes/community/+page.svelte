b710a47b68477526bef29e18df36472f564ccc55
catu <martina.provini@studenti.unimi.it>
2024-07-08 15:19:34 +0200

    community routing

---
+++ frontend/src/routes/community/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import {
+        Heading,
+        Spinner
+    } from 'flowbite-svelte';
+
+    const userId = localStorage.getItem("userId");
+
+    //cercare tutte le community dove è presente l´utente
+
+    let gruppi;
+
+    async function cercaGruppi(){
+
+    }
+    
+</script>
+
+<Heading tag="h1" class="w-full mt-6 mb-4 text-center">I tuoi gruppi</Heading>
+
+{#await gruppi}
+<div class="text-center">
+    <Spinner size={8} color="green"/>
+</div>
+{:then results}
+
+{/await}
