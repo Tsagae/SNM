da6230b0a2e970341db88d96194219e0889d260a
Matteo Zagheno <matteo2309@hotmail.com>
2024-07-09 13:57:55 +0200

    added playlist cards

--- frontend/src/routes/community/[slug]/+page.svelte
+++ frontend/src/routes/community/[slug]/+page.svelte
@@ -1,13 +1,15 @@
 <script>
     import {
-        Avatar,
+        Avatar, Button,
         Heading,
         Spinner
     } from 'flowbite-svelte';
     import {
         getCommunity,
-        getUser
+        getUser,
+        getPlaylistInfo
     } from '$lib/backend.js';
+    import Playlist from "$lib/components/playlist.svelte";
 
     let pageInfo = window.location.pathname;
     let id = pageInfo.replace("/community/", "");
@@ -20,14 +22,39 @@
         <Spinner size={8} color="green"/>
     </div>
 {:then results}
-    <Heading tag="h1" class="w-full mt-6 mb-4 text-center">Nome Gruppo</Heading>
+    <Heading tag="h1" class="w-full mt-6 mb-4 text-center">{results.communityName}</Heading>
 
     <div class="flex">
 
         <div class="w-2/3" style="background-color:red;">
 
             Playlist condivise
-
+            {#each results.sharedPlaylists as sharedPlaylist}
+                {#await getPlaylistInfo(sharedPlaylist.playlist)}
+                    <div class="text-center">
+                        <Spinner size={8} color="green"/>
+                    </div>
+                {:then playlistInfo}
+                    {#await getUser(playlistInfo.user)}
+                        <div class="text-center">
+                            <Spinner size={8} color="green"/>
+                        </div>
+                    {:then playlistCreator}
+                        <div>
+                            {#await getUser(sharedPlaylist.user)}
+                                <div class="text-center">
+                                    <Spinner size={8} color="green"/>
+                                </div>
+                            {:then sharingUser}
+                                <b>Shared by: {sharingUser.username}</b>
+                            {/await}
+                            <Playlist id={playlistInfo._id} name={playlistInfo.name} user={playlistCreator.username}
+                                      tracks={playlistInfo.tracks} tags={playlistInfo.tags}
+                                      thumbnail={playlistInfo.thumbnail} i={0}/>
+                        </div>
+                    {/await}
+                {/await}
+            {/each}
         </div>
 
         <div class="w-1/3" style="background-color:green;">
