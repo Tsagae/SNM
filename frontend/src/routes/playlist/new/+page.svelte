c9fbae1531c19a59c4658b0109fb68d237f7dd53
Matteo Zagheno <matteo2309@hotmail.com>
2024-07-09 22:14:54 +0200

    fixed create playlist from new track

--- frontend/src/routes/playlist/new/+page.svelte
+++ frontend/src/routes/playlist/new/+page.svelte
@@ -1,15 +1,15 @@
 <script>
     import {
-        Label, 
-        Heading, 
-        Input, 
-        Textarea, 
-        Toggle, 
+        Label,
+        Heading,
+        Input,
+        Textarea,
+        Toggle,
         Button,
         Spinner,
         Span
     } from 'flowbite-svelte';
-    import {createPlaylist, getTrackInfo} from '$lib/backend.js';
+    import {createPlaylist, getTrackInfo, addTrackToPlaylist} from '$lib/backend.js';
     import {goto} from "$app/navigation";
 
     const urlParams = new URLSearchParams(window.location.search);
@@ -29,9 +29,6 @@
         "tags": [],
         "description": ""
     }
-    if (urlParams.has('from')) {
-        formValues.tracks.push(urlParams.get("from"));
-    }
 
     let tagsString = "";
 
@@ -41,6 +38,9 @@
         }
         try {
             let res = await createPlaylist(formValues.name, formValues.isPublic, formValues.tracks, formValues.tags, formValues.description);
+            if (urlParams.has('from')) {
+                await addTrackToPlaylist(urlParams.get("from"), res.insertedId);
+            }
             await goto(`/playlist/${res.insertedId}`);
         } catch (e) {
             alert(e);
@@ -50,7 +50,7 @@
 
 <div class="mb-6">
     {#if urlParams.has('from')}
-        <Heading tag="h3" class="w-full text-center mt-6">Stai creando una playlist con questa canzone: 
+        <Heading tag="h3" class="w-full text-center mt-6">Stai creando una playlist con questa canzone:
             {#await getTrackInfo(urlParams.get("from"))}
                 <div class="text-center mt-16">
                     <Spinner size={8} color="green"/>
@@ -58,7 +58,7 @@
             {:then track}
                 <Span gradient>{track.name}</Span>
             {/await}
-            </Heading>
+        </Heading>
     {:else}
         <Heading tag="h3">Stai creando una playlist vuota</Heading>
     {/if}
@@ -67,11 +67,13 @@
 <form on:submit={submitForm} class="w-2/3 mx-auto">
     <div class="mb-6">
         <Label for="playlist-name" class="block mb-2">Nome</Label>
-        <Input bind:value={formValues.name} id="playlist-name" placeholder="Nome della playlist" class="block w-full text-sm text-zinc-900 border border-zinc-300 rounded-lg bg-zinc-50 focus:ring-emerald-500 focus:border-emerald-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500"/>
+        <Input bind:value={formValues.name} id="playlist-name" placeholder="Nome della playlist"
+               class="block w-full text-sm text-zinc-900 border border-zinc-300 rounded-lg bg-zinc-50 focus:ring-emerald-500 focus:border-emerald-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500"/>
     </div>
     <div class="mb-6">
         <Label for="playlist-description" class="block mb-2">Descrizione</Label>
-        <Textarea bind:value={formValues.description} for="playlist-description" {...textareaprops} class="block w-full text-sm text-zinc-900 border border-zinc-300 rounded-lg bg-zinc-50 focus:ring-emerald-500 focus:border-emerald-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500"/>
+        <Textarea bind:value={formValues.description} for="playlist-description" {...textareaprops}
+                  class="block w-full text-sm text-zinc-900 border border-zinc-300 rounded-lg bg-zinc-50 focus:ring-emerald-500 focus:border-emerald-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500"/>
     </div>
     <div class="mb-6">
         <Toggle checked={formValues.isPublic} on:click={() => formValues.isPublic = !formValues.isPublic}>Pubblica
@@ -79,7 +81,14 @@
     </div>
     <div class="mb-6">
         <Label for="playlist-tags" class="block mb-2">Tags</Label>
-        <Input bind:value={tagsString} id="playlist-tags" placeholder="Tag1,Tag2,Tag3" class="block w-full text-sm text-zinc-900 border border-zinc-300 rounded-lg bg-zinc-50 focus:ring-emerald-500 focus:border-emerald-500 dark:bg-zinc-700 dark:border-zinc-600 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-emerald-500 dark:focus:border-emerald-500"/>
+        <Input bind:value={tagsString} id="playlist-tags" placeholder="Tag1,Tag2,Tag3"/>
+    </div>
+    <div class="mb-6">
+        {#if urlParams.has('from')}
+            <p>Stai creando una playlist con questa canzone: {urlParams.get("from")}</p>
+        {:else}
+            <p>Stai creando una playlist vuota</p>
+        {/if}
     </div>
     <div class="mb-6">
         <Button type="submit">Crea</Button>
