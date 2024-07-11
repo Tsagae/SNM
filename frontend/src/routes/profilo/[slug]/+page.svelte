2d715e75f1f2981128eaf1ddabbe0ac4da99e4e0
Catu42 <93785837+Catu42@users.noreply.github.com>
2024-07-11 19:16:34 +0200

    add error message

--- frontend/src/routes/profilo/[slug]/+page.svelte
+++ frontend/src/routes/profilo/[slug]/+page.svelte
@@ -13,6 +13,7 @@
     import {myPlaylists, getUser, getUserPublicPlaylists, getArtists} from '$lib/backend.js';
     import {page} from '$app/stores';
     import LoginRequired from '$lib/components/loginrequired.svelte';
+    import ErrorMessage from '$lib/components/errormessage.svelte';
 
     let idUser = $page.params.slug;
     let imgAvatar = '';
@@ -63,12 +64,15 @@
         </div>
     {:then userInfo}
 
-        {#if userInfo.avatar}
+        {#if userInfo.error !== undefined}
+            <ErrorMessage message={userInfo.error}></ErrorMessage>
+        {:else}
+            {#if userInfo.avatar}
             <Card img={userInfo.avatar} size="lg" class="w-4/5 max-w-full m-auto mt-2 mb-6 bg-gray-100 dark:bg-zinc-700"
-                  horizontal>
+                horizontal>
                 <div class="flex">
                     <Heading tag="h1" class="mb-4"
-                             customSize="text-2xl font-extrabold md:text-5xl lg:text-6xl">{userInfo.username}</Heading>
+                            customSize="text-2xl font-extrabold md:text-5xl lg:text-6xl">{userInfo.username}</Heading>
                     {#if userInfo._id === localStorage.getItem("userId")}
                         <DotsHorizontalOutline class="dots-menu dark:text-white"/>
                         <Dropdown triggeredBy=".dots-menu" class="bg-gray-200 dark:bg-zinc-600">
@@ -108,52 +112,53 @@
                         {/if}
                     {/await}
                 {/if}
-            </Card>
-        {:else}
-            <Card class="w-4/5 max-w-full m-auto mt-2 mb-6 bg-gray-100 dark:bg-zinc-700">
-                <div class="flex">
-                    <Heading tag="h1" class="mb-4 flex"
-                             customSize="text-2xl font-extrabold md:text-5xl lg:text-6xl">{userInfo.username}</Heading>
-                    {#if userInfo._id === localStorage.getItem("userId")}
-                        <DotsHorizontalOutline class="dots-menu dark:text-white"/>
-                        <Dropdown triggeredBy=".dots-menu" class="bg-gray-200 dark:bg-zinc-600">
-                            <DropdownItem href="/profilo/edit">Modifica</DropdownItem>
-                        </Dropdown>
-                    {/if}
-                </div>
-                <p class="text-2xl dark:text-white">Preferenze</p>
-                <div class="mt-2">
-                    {#if userInfo.genres}
-                        {#each userInfo.genres as genre}
-                            <Badge rounded color="green" class="mx-1">#{genre}</Badge>
-                        {/each}
-                    {/if}
-                </div>
-                {#if userInfo.artists}
-                    {#await getArtists(userInfo.artists)}
-                        <div class="text-center mt-16">
-                            <Spinner size={8} color="green"/>
-                        </div>
-                    {:then artistsRes}
-                        {#if artistsRes.error !== undefined}
-                            <p>{artistsRes.error}</p>
-                        {:else}
-                            <ul class="flex flex-wrap mt-4 text-gray-900 dark:text-white">
-                                {#each artistsRes.artists as artist}
-                                    <li class="mx-2">
-                                        <div class="flex items-center space-x-2 rtl:space-x-reverse">
-                                            <Avatar src="{artist.images[0].url}" rounded/>
-                                            <div class="space-y-1 font-medium dark:text-white">
-                                                <div>{artist.name}</div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                {/each}
-                            </ul>
+                </Card>
+            {:else}
+                <Card class="w-4/5 max-w-full m-auto mt-2 mb-6 bg-gray-100 dark:bg-zinc-700">
+                    <div class="flex">
+                        <Heading tag="h1" class="mb-4 flex"
+                                customSize="text-2xl font-extrabold md:text-5xl lg:text-6xl">{userInfo.username}</Heading>
+                        {#if userInfo._id === localStorage.getItem("userId")}
+                            <DotsHorizontalOutline class="dots-menu dark:text-white"/>
+                            <Dropdown triggeredBy=".dots-menu" class="bg-gray-200 dark:bg-zinc-600">
+                                <DropdownItem href="/profilo/edit">Modifica</DropdownItem>
+                            </Dropdown>
                         {/if}
-                    {/await}
-                {/if}
-            </Card>
+                    </div>
+                    <p class="text-2xl dark:text-white">Preferenze</p>
+                    <div class="mt-2">
+                        {#if userInfo.genres}
+                            {#each userInfo.genres as genre}
+                                <Badge rounded color="green" class="mx-1">#{genre}</Badge>
+                            {/each}
+                        {/if}
+                    </div>
+                    {#if userInfo.artists}
+                        {#await getArtists(userInfo.artists)}
+                            <div class="text-center mt-16">
+                                <Spinner size={8} color="green"/>
+                            </div>
+                        {:then artistsRes}
+                            {#if artistsRes.error !== undefined}
+                                <p>{artistsRes.error}</p>
+                            {:else}
+                                <ul class="flex flex-wrap mt-4 text-gray-900 dark:text-white">
+                                    {#each artistsRes.artists as artist}
+                                        <li class="mx-2">
+                                            <div class="flex items-center space-x-2 rtl:space-x-reverse">
+                                                <Avatar src="{artist.images[0].url}" rounded/>
+                                                <div class="space-y-1 font-medium dark:text-white">
+                                                    <div>{artist.name}</div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    {/each}
+                                </ul>
+                            {/if}
+                        {/await}
+                    {/if}
+                </Card>
+            {/if}    
         {/if}
     {/await}
 
--- frontend/src/routes/track/[slug]/+page.svelte
+++ frontend/src/routes/track/[slug]/+page.svelte
@@ -9,6 +9,7 @@
         Spinner
     } from 'flowbite-svelte';
     import {addTrackToPlaylist, getTrackInfo, myPlaylists, isValidToken} from '$lib/backend.js';
+    import ErrorMessage from '$lib/components/errormessage.svelte';
     import {ChevronDownOutline, PlusOutline, PauseSolid, PlaySolid, InfoCircleSolid} from "flowbite-svelte-icons";
     import {goto} from "$app/navigation";
     import { page } from '$app/stores';
@@ -71,67 +72,71 @@
         <Spinner size={8} color="green"/>
     </div>
 {:then track}
-    <Card img={track.album.images[0].url} class="w-4/5 max-w-full m-auto mt-6 mb-2 bg-gray-100 dark:bg-zinc-700" horizontal>
-        <p>Canzone</p>
-        <br>
-        <Heading tag="h1" class="mb-4" customSize="text-2xl font-extrabold md:text-5xl lg:text-6xl">{track.name}</Heading>
-        <br><br>
-        {#each track.artists as artist}
-            {artist.name}&nbsp;
-        {/each} - &nbsp;
-        <b>{track.album.name}</b>, {track.album.release_date}
-    </Card>
-    
-    <div class="w-4/5 max-w-full m-auto mt-6 mb-2 flex justify-center">
+    {#if track.error !== undefined}
+        <ErrorMessage message={track.error}></ErrorMessage>
+    {:else}
+        <Card img={track.album.images[0].url} class="w-4/5 max-w-full m-auto mt-6 mb-2 bg-gray-100 dark:bg-zinc-700" horizontal>
+            <p>Canzone</p>
+            <br>
+            <Heading tag="h1" class="mb-4" customSize="text-2xl font-extrabold md:text-5xl lg:text-6xl">{track.name}</Heading>
+            <br><br>
+            {#each track.artists as artist}
+                {artist.name}&nbsp;
+            {/each} - &nbsp;
+            <b>{track.album.name}</b>, {track.album.release_date}
+        </Card>
+        
+        <div class="w-4/5 max-w-full m-auto mt-6 mb-2 flex justify-center">
 
-        <Rating id="example-1b" total={5} size={50} rating={(track.popularity*5)/100}> <p slot="text" class="ms-2 text-sm font-medium text-gray-500 dark:text-gray-400">Popolarità: {(track.popularity*5)/100} su 5</p> </Rating> 
-    
-    </div>
+            <Rating id="example-1b" total={5} size={50} rating={(track.popularity*5)/100}> <p slot="text" class="ms-2 text-sm font-medium text-gray-500 dark:text-gray-400">Popolarità: {(track.popularity*5)/100} su 5</p> </Rating> 
+        
+        </div>
 
-    <div class="w-4/5 max-w-full m-auto mt-6 mb-2 flex">
+        <div class="w-4/5 max-w-full m-auto mt-6 mb-2 flex">
 
-        <Button on:click={() => playSelectedSong(track.preview_url)} pill shadow>      
-            {#if playingState == 'playing'}
-                <PauseSolid class="mr-2"/> {Math.floor(track.duration_ms / 60000)}:{(((track.duration_ms % 60000) / 1000).toFixed(0)) < 10 ? '0' : ''}{((track.duration_ms % 60000) / 1000).toFixed(0)}
-            {:else}
-                <PlaySolid class="mr-2"/> {Math.floor(track.duration_ms / 60000)}:{(((track.duration_ms % 60000) / 1000).toFixed(0)) < 10 ? '0' : ''}{((track.duration_ms % 60000) / 1000).toFixed(0)}
+            <Button on:click={() => playSelectedSong(track.preview_url)} pill shadow>      
+                {#if playingState == 'playing'}
+                    <PauseSolid class="mr-2"/> {Math.floor(track.duration_ms / 60000)}:{(((track.duration_ms % 60000) / 1000).toFixed(0)) < 10 ? '0' : ''}{((track.duration_ms % 60000) / 1000).toFixed(0)}
+                {:else}
+                    <PlaySolid class="mr-2"/> {Math.floor(track.duration_ms / 60000)}:{(((track.duration_ms % 60000) / 1000).toFixed(0)) < 10 ? '0' : ''}{((track.duration_ms % 60000) / 1000).toFixed(0)}
+                {/if}
+            </Button> 
+        
+            {#if logged}
+                <div class="flex flex-col items-center ml-4">
+                    <Button color="primary" pill>
+                        <PlusOutline class="w-6 h-6 mr-2 text-white dark:text-white"/>
+                        Aggiungi a una playlist
+                        <ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white"/>
+                    </Button>
+                    <Dropdown class="overflow-y-auto px-3 pb-3 text-sm">
+                        {#await playlistsPromise}
+                            <div class="text-center mt-16">
+                                <Spinner size={8} color="green"/>
+                            </div>
+                        {:then playlists}
+                            {#each playlists as playlist}
+                                {#if !playlist.tracks.find(o => o.id === trackId)}
+                                    <li class="rounded p-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600">
+                                        <Button on:click={async () => {
+                                            await addTrackToPlaylist(trackId, playlist._id);
+                                            trackAdded = true;
+                                            }} class="w-full">{playlist.name}</Button>
+                                    </li>
+                                {/if}
+                            {/each}
+                            <li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+                                <Button on:click={() => goto(`/playlist/new?from=${track.id}`)}>
+                                    <PlusOutline class="w-6 h-6 mr-2 text-white dark:text-white"/>
+                                    Nuova playlist
+                                </Button>
+                            </li>
+                        {/await}
+                    </Dropdown>
+                </div>
             {/if}
-        </Button> 
-    
-        {#if logged}
-            <div class="flex flex-col items-center ml-4">
-                <Button color="primary" pill>
-                    <PlusOutline class="w-6 h-6 mr-2 text-white dark:text-white"/>
-                    Aggiungi a una playlist
-                    <ChevronDownOutline class="w-6 h-6 ms-2 text-white dark:text-white"/>
-                </Button>
-                <Dropdown class="overflow-y-auto px-3 pb-3 text-sm">
-                    {#await playlistsPromise}
-                        <div class="text-center mt-16">
-                            <Spinner size={8} color="green"/>
-                        </div>
-                    {:then playlists}
-                        {#each playlists as playlist}
-                            {#if !playlist.tracks.find(o => o.id === trackId)}
-                                <li class="rounded p-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600">
-                                    <Button on:click={async () => {
-                                        await addTrackToPlaylist(trackId, playlist._id);
-                                        trackAdded = true;
-                                        }} class="w-full">{playlist.name}</Button>
-                                </li>
-                            {/if}
-                        {/each}
-                        <li class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
-                            <Button on:click={() => goto(`/playlist/new?from=${track.id}`)}>
-                                <PlusOutline class="w-6 h-6 mr-2 text-white dark:text-white"/>
-                                Nuova playlist
-                            </Button>
-                        </li>
-                    {/await}
-                </Dropdown>
-            </div>
-        {/if}
-    </div>
+        </div>
+    {/if}
 {/await}
 
 {#if trackAdded}            
